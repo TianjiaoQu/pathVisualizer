47a34543dd10b41ef96e59e7eaa9001c761b6a31
TianjiaoQu <qtianjiao@gmail.com>
2020-09-18 14:07:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./App.css";
 import HighlightIcon from "@material-ui/icons/Highlight";
 import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import introJs from 'intro.js';
+import 'intro.js/introjs.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import Icon from './basic/Icon';
+// using intro.js
+const intro = introJs();
 
 function App() {
   return (
@@ -11,6 +18,9 @@ function App() {
           <HighlightIcon />
           Path Visualizer
         </h1>
+        <Icon touchRadius="8" onClick={() => intro.start()} color="#7f00c4">
+            <FontAwesomeIcon icon={faInfoCircle} />
+          </Icon>
       </header>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
